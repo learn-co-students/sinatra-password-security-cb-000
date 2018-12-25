4726d831b1a6e3318802ba2307b4b0e7070738c8
kenkuts <kennethchoi19@gmail.com>
2018-12-25 12:16:33 -0500

    created password_presist column

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180314210852) do
+ActiveRecord::Schema.define(version: 20181225040704) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
+    t.string "password_digest"
   end
 
 end
